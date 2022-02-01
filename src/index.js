13f16856aaeac284d0dcca0dfddfbaf46a6abcd7
dmitry <Villigo701@gmail.com>
2022-02-01 14:59:52 +0300

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let handledStr = str;
+
+  const brackets = [];
+
+  bracketsConfig.forEach(bracket => {
+    brackets.push(bracket.join(''))
+  });
+
+  for (let i = 0; i < brackets.length;) {
+    if (handledStr.includes(brackets[i])) {
+      handledStr = handledStr.replace(brackets[i], '');
+      i = 0;
+    } else {
+      i++;
+    }
+  }
+
+  return handledStr ? false : true;
+
 }
